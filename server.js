22d79327311fa7e8972c00da92015d8021a08f9f
randze <65006180+randze@users.noreply.github.com>
2020-08-02 03:34:27 -0400

    Base files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// // Serve up static assets
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
+// routes
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './client/build/'));
+// });
+
+// listener
+app.listen(`🌎 ==> API server now on port ${PORT}!`);
