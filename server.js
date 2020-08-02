2d4d2b8d861a0714ca5552924ec1aa5632dd7e70
randze <65006180+randze@users.noreply.github.com>
2020-08-02 03:23:15 -0400

    Another test

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+var cors = require('cors');
+
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-// Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
+// // Serve up static assets
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
 // routes
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './client/build/'));
+// });
 
 // listener
 app.listen(`🌎 ==> API server now on port ${PORT}!`);
